08d29d87da47c9ab3c5849706b01b676d96b4e9f
realisticattorney <g.axl.aquila@gmail.com>
2021-06-16 01:04:51 -0300

    Add examples of books

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './components/App';
 
+const initialStore = {
+  books: [
+    {
+      author: 'Nassim Taleb',
+      title: 'Skin in the game',
+      category: 'Learning',
+    },
+    {
+      author: 'Antoine de Saint-Exupéry',
+      title: 'The Little Prince',
+      category: 'Kids',
+    },
+  ],
+};
+
+const store = createStore(reducer, initialStore);
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
+  </Provider>,
+  document.getElementById('root')
 );
